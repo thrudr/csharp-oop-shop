b8d6c42fd43611eeeb4de55fbf4561aa5fbf296c
thrudr <ariel.oliveiraesilva@gmail.com>
2023-04-18 16:11:57 +0200

    constructors-randomCode-with-getter

--- OopShop/OopShop.cs
+++ OopShop/OopShop.cs
@@ -6,7 +6,45 @@ using System.Threading.Tasks;
 
 namespace OopShop
 {
-    internal class OopShop
+    public class Article
     {
+        // FEATURES
+        private int code;
+        private string name;
+        private string description;
+        private float price;
+        private int iva;
+
+        // CONSTRUCTORS
+
+        public Article()
+        {
+            this.code = RandomCode();
+        }
+
+        public Article(string name, string description, float price, int iva)
+        {
+            this.code = RandomCode();
+            this.name = name;
+            this.description = description;
+            this.price = price;
+            this.iva = iva;
+        }
+
+        // GETTERS
+
+        public int GetCode()
+        {
+            return this.code;
+        }
+
+        //METHODS
+
+        private int RandomCode()
+        {
+            Random random = new Random();
+            int RandomCode = random.Next(99999999 + 1);
+            return RandomCode;
+        }
     }
 }
